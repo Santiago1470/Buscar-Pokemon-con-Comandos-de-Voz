aeb2fea519572ed4e696e6f144b0ec975f910d0f
Invitado <invitado@gmail.com>
2024-03-14 14:58:06 -0500

    Mayúscula la inicial del nombre del pokemon

--- js/script-index.js
+++ js/script-index.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
             url: "https://pokeapi.co/api/v2/pokemon/" + $("#txt_buscador").val(),
             contentType: "application/json",
             success: function (data) {
-                console.log(data.name)
+                console.log(data)
                 $(".imagen").html(`<img src="${data.sprites.other.home.front_default}" width="250px">`)
                 $(".card-title").text(`${data.name}`);
             }
