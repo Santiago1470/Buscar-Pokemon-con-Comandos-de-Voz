ec534fde61f2339f7349e633d6c4bb513d9051a3
Santiago1470 <st147086@gmail.com>
2024-03-17 00:29:23 -0500

    Creación de la tarjeta del pokémon desde jquery

--- js/script-index.js
+++ js/script-index.js
@@ -5,8 +5,45 @@ $(document).ready(function () {
             contentType: "application/json",
             success: function (data) {
                 console.log(data)
-                $(".imagen").html(`<img src="${data.sprites.other.home.front_default}" width="250px">`)
-                $(".card-title").text(`${data.name}`);
+                // $(".imagen").html(`<img src="${data.sprites.other.home.front_default}" width="250px">`)
+                // $(".info-p-pokemon").html(`<p class="card-text">Experiencia básica: ${data.base_experience}</p><p class="card-text">Altura: ${data.height}</p><p class="card-text">Peso: ${data.weight}</p>`)
+
+                // $(".card-title").text(`${data.name}`)
+                // $(".base-experience").text(`Experiencia básica: ${data.base_experience}`)
+                // $(".height").text(`Altura: ${data.height}`)
+                // $(".weight").text(`Peso: ${data.base_experience}`)
+                // $(".base-experience").append(` ${data.base_experience}`)
+                // $(".info-p-pokemon").append(`<p class="card-text">Altura: ${data.height}<p/>`);
+                // $(".info-p-pokemon").append(`<p class="card-text">Peso: ${data.weight}<p/>`);
+                // $(".info-p-pokemon").html(`<p class="card-text">Experiencia básica: ${data.base_experience}</p>
+                // <p class="card-text">Altura: ${data.height}<p/>
+                // <p class="card-text">Peso: ${data.weight}<p/>`)
+
+                var cardPokemon = `
+                <div class="card card-pokemon">
+                    <div class="imagen">
+                        <img src="${data.sprites.other.home.front_default}" width="250px">
+                    </div>
+                        <!-- <img class="card-img-top" alt="Pokemon"> -->
+                    <div class="card-body info-p-pokemon">
+                        <h5 class="card-title titulo-card">${data.name}</h5>
+                        <p class="card-text">Experiencia básica: ${data.base_experience}</p>
+                        <p class="card-text">Altura: ${data.height}</p>
+                        <p class="card-text">Peso: ${data.weight}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Tipo: ${data.types[0].type.name}</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <!-- <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div> -->
+                </div>
+                `
+                $(".contenedor-card").html(cardPokemon)
+                
             }
         })
     })
