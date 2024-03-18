3115565196b81f9e4530b09923510c9d6335b77f
Andres Patarroyo <A@gmail.com>
2024-03-18 17:54:23 -0500

    Arreglo del pokemon mostrado y cambio de color de botón

--- js/script-index.js
+++ js/script-index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("#buscar").on("click", () => {
         $.ajax({
-            url: "https://pokeapi.co/api/v2/pokemon/" + $("#txt_buscador").val(),
+            url: "https://pokeapi.co/api/v2/pokemon/" + ($("#txt_buscador").val()).toLowerCase(),
             contentType: "application/json",
             success: function (data) {
                 console.log(data)
@@ -28,16 +28,16 @@ $(document).ready(function () {
                         <!-- <img class="card-img-top" alt="Pokemon"> -->
                     <div class="card-body info-p-pokemon">
                         <h5 class="card-title titulo-card">${data.name}</h5>
-                        <p class="card-text">Experiencia básica: ${data.base_experience}</p>
                         <p class="card-text">Altura: ${data.height}</p>
                         <p class="card-text">Peso: ${data.weight}</p>
                     </div>
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Tipo: ${data.types[0].type.name}</li>
-                        <li class="list-group-item">hp: ${data.stats[0].base_stat}</li>
-                        <li class="list-group-item">ataque: ${data.stats[1].base_stat}</li>
-                        <li class="list-group-item">defensa: ${data.stats[2].base_stat}</li>
-                        <li class="list-group-item">velocidad: ${data.stats[5].base_stat}</li>
+                        <ul class="list-group list-group-flush">        </ul>
+                        <li class="list-group-item">Puntos de Vida: ${data.stats[0].base_stat}</li>
+                        <li class="list-group-item">Puntos de Ataque: ${data.stats[1].base_stat}</li>
+                        <li class="list-group-item">Puntos de Defensa: ${data.stats[2].base_stat}</li>
+                        <li class="list-group-item">Velocidad: ${data.stats[5].base_stat}</li>
                     </ul>
                     <!-- <div class="card-body">
                         <a href="#" class="card-link">Card link</a>
@@ -45,32 +45,7 @@ $(document).ready(function () {
                     </div> -->
                 </div>
                 `;
-
-                var cardPokemon2 = `
-                <div class="card mb-3 " style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img class="img-fluid rounded-start" src="${data.sprites.other.home.front_default}">
-                    </div>
-                        <!-- <img class="card-img-top" alt="Pokemon"> -->
-                    <div class="col-md-8">
-                        <div class="card-body">
-                        <h5 class="card-title titulo-card">${data.name}</h5>
-                        <p class="card-text">Experiencia básica: ${data.base_experience}</p>
-                        <p class="card-text">Altura: ${data.height}</p>
-                        <p class="card-text">Peso: ${data.weight}</p>
-                        <p class="list-group-item">Tipo: ${data.types[0].type.name}</p>
-                        <p class="list-group-item">hp: ${data.stats[0].base_stat}</p>
-                        <p class="list-group-item">ataque: ${data.stats[1].base_stat}</p>
-                        <p class="list-group-item">defensa: ${data.stats[2].base_stat}</p>
-                        <p class="list-group-item">velocidad: ${data.stats[5].base_stat}</p>
-                        </div>
-                    </div>
-                    
-                </div>
-                </div>
-                `;
-                $(".contenedor-card").html(cardPokemon2)
+                $(".contenedor-card").html(cardPokemon)
 
             }
         }).fail((jqXHR, textStatus, errorThrown) => {
