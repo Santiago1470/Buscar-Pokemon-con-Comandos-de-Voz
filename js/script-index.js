76bcd938e48843fd7ebe8f5368c19b0070e6a777
Andres Felipe Patarroyo <andresf.patarroyom@konradlorenz.edu.co>
2024-03-24 13:21:27 -0500

    Autocompletar nombres pokemon, pero abajo se crea un espacio que son los resultados tambien

--- js/script-index.js
+++ js/script-index.js
@@ -14,10 +14,11 @@ const tipoPorColor = {
     'electric': '#FFD700',
     'psychic': '#FF69B4',
     'ice': '#B0E0E6',
-    'dragon': '#B8860B', 
-    'dark': '#000000', 
+    'dragon': '#B8860B',
+    'dark': '#000000',
     'fairy': '#D8BFD8'
 };
+
 
 $(document).ready(function () {
     $("#buscar").on("click", () => {
@@ -28,12 +29,13 @@ $(document).ready(function () {
                 console.log(data)
                 let tipo = data.types[0].type.name;
                 let colorTipo = tipoPorColor[tipo];
-                let mov1= (data.abilities[0].ability.name).toUpperCase();
-                let mov2 =(data.abilities[1].ability.name).toUpperCase();
+                let mov1 = (data.abilities[0].ability.name).toUpperCase();
+                let mov2 = data.abilities.length > 1 ? (data.abilities[1].ability.name).toUpperCase() : "No existe";
                 var cardPokemon = `
                 <div class="card card-pokemon">
                     <div class="imagen">
-                        <img src="${data.sprites.other.home.front_default}" width="250px" id="pokemon">
+                    <img src="${normal}" width="250px" id="pokemon">
+                    <button id="toggleImageBtn">Cambiar a shiny</button>
                     </div>
                     <div class="card-body info-p-pokemon">
                         <h5 class="card-title titulo-card">${data.name}</h5>
@@ -53,38 +55,35 @@ $(document).ready(function () {
                 </div>
                 `;
                 $(".contenedor-card").html(cardPokemon);
-                $(".card-pokemon").css("box-shadow", `10px 7px 10px ${colorTipo}`); 
-                $("#pokemon").css("filter", `drop-shadow(10px 7px 10px ${colorTipo})`); 
-                $(".card-pokemon").css("background-color", `${colorTipo}90`); 
+                $(".card-pokemon").css("box-shadow", `10px 7px 10px ${colorTipo}`);
+                $("#pokemon").css("filter", `drop-shadow(10px 7px 10px ${colorTipo})`);
+                $(".card-pokemon").css("background-color", `${colorTipo}90`);
             }
         }).fail((jqXHR, textStatus, errorThrown) => {
             if (jqXHR.status == "404") {
                 let nombreP = $("#txt_buscador").val();
-                let mensaje = `
-                <div class="modal" tabindex="-1">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                            <h5 class="modal-title">Modal title</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                            <p>Modal body text goes here.</p>
-                            </div>
-                            <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                `;
-
                 $("#modalError").attr("class", "modal fade show").attr("style", "display: block");
                 $(".modal-body").html(`<img src="files/error.png"><p>El pokémon con el nombre <strong>${nombreP}</strong> no existe.</p>`);
             }
         });
     });
+    $("#txt_buscador").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: "https://pokeapi.co/api/v2/pokemon/?limit=1000",
+                dataType: "json",
+                success: function (data) {
+                    var pokemonNames = data.results.map(function (pokemon) {
+                        return pokemon.name;
+                    });
+                    var filteredNames = $.ui.autocomplete.filter(pokemonNames, request.term);
+                    response(filteredNames.slice(0, 5));
+                }
+            });
+        },
+        minLength: 1,
+        appendTo: ".autocompletar"
+    }).autocomplete("widget").addClass("autocompletar");
 });
 
 var txtBuscador = document.querySelector("#txt_buscador");
