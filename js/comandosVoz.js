34b024d06be7e74113097914ff16f027b8756246
Santiago1470 <st147086@gmail.com>
2024-03-24 22:45:07 -0500

    Opción para buscar pokémon por voz
    Se cambió la activación de los controles de voz por el botón de "Buscar por voz" para activar los comandos de voz y decir inmediatamente el nombre del pokémon, una vez dicho, se desactivarán los comandos de voz.

--- js/comandosVoz.js
+++ js/comandosVoz.js
@@ -8,38 +8,38 @@ var activarComandos = document.querySelector("#activar");
 var activado = false;
 activarComandos.addEventListener('click', function () {
     if (!activado) {
-        artyom.say("Comandos de voz activados");
+        //artyom.say("Comandos de voz activados");
         artyom.obey();
-        activarComandos.textContent = "Desactivar comandos de voz";
+        //activarComandos.textContent = "Desactivar comandos de voz";
         activado = true;
     } else {
-        artyom.say("Comandos de voz desactivados");
-        artyom.dontObey();
-        activarComandos.textContent = "Activar comandos de voz";
-        activado = false;
+        // artyom.say("Comandos de voz desactivados");
+        // artyom.dontObey();
+        // activarComandos.textContent = "Activar comandos de voz";
+        // activado = false;
     }
 });
 
 artyom.addCommands({
-    indexes: ["comando", "activar comandos de voz", "desactivar comandos de voz", "buscar"],
+    indexes: ["comando"/*, "activar comandos de voz", "desactivar comandos de voz", "buscar"*/],
     action: function (i) {
         if (i == 0) {
             console.log("recibido");
 
-        } else if(i == 1){
-            artyom.obey();
-            artyom.say("Comandos de voz activados");
-            activarComandos.textContent = "Desactivar comandos de voz";
-        } else if (i == 2) {
-            artyom.dontObey();
-            artyom.say("Comandos de voz desactivados");
-            activarComandos.textContent = "Activar comandos de voz";
-        } else if(i == 3){
-            let txtBuscador = document.querySelector("#txt_buscador");
-            let buscar = textoPorVoz.split(" ");
-            txtBuscador.value = buscar[1].toLowerCase();
-            document.querySelector("#buscar").click();
-        }
+        }// else if(i == 1){
+        //     artyom.obey();
+        //     artyom.say("Comandos de voz activados");
+        //     activarComandos.textContent = "Desactivar comandos de voz";
+        // } else if (i == 2) {
+        //     artyom.dontObey();
+        //     artyom.say("Comandos de voz desactivados");
+        //     activarComandos.textContent = "Activar comandos de voz";
+        // } else if(i == 3){
+        //     let txtBuscador = document.querySelector("#txt_buscador");
+        //     let buscar = textoPorVoz.split(" ");
+        //     txtBuscador.value = buscar[1].toLowerCase();
+        //     document.querySelector("#buscar").click();
+        // }
     }
 });
 
@@ -50,7 +50,8 @@ artyom.initialize({
     continuous: true,
     speed: 1,
     mode: "normal",
-    obeyKeyword: "activar comandos de voz"
+    oberyKeyword: "Buscar por voz"
+    // obeyKeyword: "activar comandos de voz"
 });
 
 var textoPorVoz = "";
@@ -58,6 +59,11 @@ artyom.redirectRecognizedTextOutput(function (recognized, isFinal) {
     if (isFinal) {
         console.log("Texto final reconocido: " + recognized);
         textoPorVoz = recognized;
+        let txtBuscador = document.querySelector("#txt_buscador");
+            txtBuscador.value = textoPorVoz.trim();
+            document.querySelector("#buscar").click();
+        artyom.dontObey();
+        activado = false;
     } else {
         console.log(recognized);
     }
