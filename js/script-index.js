fac15a6ca5da94ffbca9797fd1dc0803b3196182
Andres Felipe Patarroyo <andresf.patarroyom@konradlorenz.edu.co>
2024-03-25 18:03:20 -0500

    Cambio de imagen a imagen HD.

--- js/script-index.js
+++ js/script-index.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
                 var cardPokemon = `
                 <div class="card card-pokemon">
                     <div class="imagen">
-                    <img src="${data.sprites.front_default}" width="250px" id="pokemon">
+                    <img src="${data.sprites.other.home.front_default}" width="250px" id="pokemon">
                     </div>
                     <div class="card-body info-p-pokemon">
                         <h5 class="card-title titulo-card">${data.name}</h5>
